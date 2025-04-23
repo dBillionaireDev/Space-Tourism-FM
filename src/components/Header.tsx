359cfc19214ca7dc70092329b0aafbe645c53c5b
dBillionaireDev <nezerekunke@gmail.com>
2025-04-23 20:09:29 +0100

    First

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            00 Home
+            01 Destination
+            02 Crew
+            03 Technology
+        </div>
+    )
+}
+
+export default Header
